0ca0fd39072dcbb409bd091b978f729425ee27e1
Mary Saray <msaray.ms@outlook.com>
2021-07-11 16:23:45 -0700

    Add project files.

---
+++ SourceControlVS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SourceControlVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
