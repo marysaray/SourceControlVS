440489a30ead9ceec31b5bb8244cc01485b99425
Mary Saray <msaray.ms@outlook.com>
2021-07-11 16:52:43 -0700

    Added prompt message.

--- SourceControlVS/Program.cs
+++ SourceControlVS/Program.cs
@@ -7,6 +7,9 @@ namespace SourceControlVS
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Advanced .Net Programming!");
+
+            Console.WriteLine("Press any key to end.");
+            Console.ReadKey();
         }
     }
 }
