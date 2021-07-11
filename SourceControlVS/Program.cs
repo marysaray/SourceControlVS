8426c8c0ced6911a040eaea5d09defaeb8fda305
Mary Saray <msaray.ms@outlook.com>
2021-07-11 16:46:20 -0700

    Changed initial message.

--- SourceControlVS/Program.cs
+++ SourceControlVS/Program.cs
@@ -6,7 +6,7 @@ namespace SourceControlVS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Advanced .Net Programming!");
         }
     }
 }
