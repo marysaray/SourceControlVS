48e1f3ca1db63bf8448b97f79cfd22a484386a94
Mary Saray <msaray.ms@outlook.com>
2021-07-11 22:23:33 -0700

    Created StudentDB with method stub.

---
+++ SourceControlVS/StudentDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SourceControlVS
+{
+    /// <summary>
+    /// DB Helper class for Student CRUD functionality.
+    /// </summary>
+    static class StudentDB
+    {
+        /// <summary>
+        /// Get a list of students from the database.
+        /// </summary>
+        /// <returns></returns>
+        public static List<string> GetAllStudents()
+        {
+            throw new NotFiniteNumberException();
+        }
+    }
+}
