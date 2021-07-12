259e4c4c77cef4db0495de7cb84641604d58864b
Mary Saray <msaray.ms@outlook.com>
2021-07-11 22:37:51 -0700

    Created Student class for object-oriented program.

---
+++ SourceControlVS/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SourceControlVS
+{
+    /// <summary>
+    /// Represents an Individual Student
+    /// </summary>
+    class Student
+    {
+        /// <summary>
+        /// The 9 digit Student Identification number
+        /// </summary>
+        public string StudentID { get; set; }
+
+        /// <summary>
+        /// Legal Student First Name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Legal Student Last Name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Email associated with the student
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
