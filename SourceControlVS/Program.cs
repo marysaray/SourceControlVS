c1b32555059c5dc46fe9e4425bec9e219220f97f
Mary Saray <msaray.ms@outlook.com>
2021-07-11 18:46:22 -0700

    Added a message method and documentation.

--- SourceControlVS/Program.cs
+++ SourceControlVS/Program.cs
@@ -12,8 +12,19 @@ namespace SourceControlVS
         {
             Console.WriteLine("Welcome to Advanced .Net Programming!");
 
+            DisplayMessage();
+
             Console.WriteLine("Press any key to end.");
             Console.ReadKey();
+        }
+        /// <summary>
+        /// This method refers to the Issue#1 on GitHub for creating a new branch.
+        /// </summary>
+        static void DisplayMessage()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Completed the new branch exercise.");
+            Console.WriteLine();
         }
     }
 }
