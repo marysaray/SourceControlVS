fe4dc0b3fad6d81755d58584972a513e4bfd8e09
Mary Saray <msaray.ms@outlook.com>
2021-07-11 19:50:16 -0700

    Created database script for StudentDB.

---
+++ StudentDB/StudentDB.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE StudentDB
+GO
+
+USE StudentDB
+GO
+
+CREATE TABLE Students
+(
+    StudentID  char(9)        NOT NULL PRIMARY KEY,
+    FirstName  varchar(30)    NOT NULL,
+    LastName   varchar(30)    NOT NULL,
+    Email      varchar(150)   NOT NULL
+)
